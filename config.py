3ab6050c9975f19fb5c151a979dc4618ea9f7470
palich <rautaruukkipalich@gmail.com>
2024-02-01 00:19:08 +0500

    add redis settings

--- config.py
+++ config.py
@@ -15,4 +15,7 @@ DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASS = os.environ.get('DB_PASS', 'postgres')
 
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+
 DB_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
