042e2714a7b460c41ba091b69f6df8f05562ca53
palich <rautaruukkipalich@gmail.com>
2024-02-06 16:38:47 +0500

    delete unused params

--- config.py
+++ config.py
@@ -5,12 +5,8 @@ load_dotenv()
 
 TOKEN = str(os.environ.get("TG_TOKEN"))
 
-HOST_ADD = os.environ.get("HOST_ADD", "web")
-HOST_PORT = os.environ.get("HOST_PORT", 8080)
-HOST_URL = f"http://{HOST_ADD}:{HOST_PORT}"
-
 DB_NAME = os.environ.get('DB_NAME', 'postgres')
-DB_HOST = os.environ.get('DB_HOST', 'database')
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
 DB_PORT = os.environ.get('DB_PORT', '5432')
 DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASS = os.environ.get('DB_PASS', 'postgres')
