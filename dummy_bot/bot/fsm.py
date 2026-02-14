0a754ca505aed8860b723c033c0313652434baae
palich <rautaruukkipalich@gmail.com>
2026-02-15 01:31:19 +0500

    delete old version

--- dummy_bot/bot/fsm.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class SetMedia(StatesGroup):
-    get_media = State()
--- dummy_bot/middlewares/db_middleware.py
+++
@@ -1,7 +0,0 @@
-from typing import Callable, Awaitable, Dict, Any
-
-from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject
-from sqlalchemy.ext.asyncio import async_sessionmaker
-
-
